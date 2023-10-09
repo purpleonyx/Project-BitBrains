bfb39c924dab90d74292f752276a6356b701f856
Odilbek <146552216+purpleonyx@users.noreply.github.com>
2023-10-09 11:04:19 +0100

    good so far

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'scripts/data.json'
+  })
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('dev-anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'scripts/developer_anim/developer.json'
+  })
