c69cc4ae72c38ac94c8f92bfac55207940cef3c7
Odilbek <146552216+purpleonyx@users.noreply.github.com>
2023-10-17 10:19:01 +0100

    first responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,13 @@ var animation = bodymovin.loadAnimation({
     autoplay: true,
     path: 'scripts/developer_anim/developer.json'
   })
+
+  function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+  }
